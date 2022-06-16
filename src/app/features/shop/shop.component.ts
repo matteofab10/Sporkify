eea099d4ca5c5dda925945ea97f566fc59572fe5
giorgio_carpoca <g.carpoca@gmail.com>
2022-06-16 14:57:42 +0200

    form complete

--- src/app/features/shop/shop.component.ts
+++ src/app/features/shop/shop.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Abbonamento} from "../../core/models/abbonamento";
 import {User} from "../../core/models/user";
-import {FormControl, FormGroup, NgForm} from "@angular/forms";
+import {FormControl, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-shop',
@@ -10,30 +10,32 @@ import {FormControl, FormGroup, NgForm} from "@angular/forms";
 })
 export class ShopComponent implements OnInit {
 
-  listAbbonamenti: Abbonamento[] = [
-    {
-      name: "Basic",
-      cost: 7.99,
-      amount: 1
-    },
-    {
-      name: "Premium",
-      cost: 12.99,
-      amount: 3
-    },
-    {
-      name: "Family",
-      cost: 19.99,
-      amount: 6
-    }
-  ];
-
+  listAbbonamenti: Abbonamento[] = []
   userForm: FormGroup;
   user : User;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.listAbbonamenti = [
+      {
+        name: "Basic",
+        cost: 7.99,
+        amount: 1
+      },
+      {
+        name: "Premium",
+        cost: 12.99,
+        amount: 3
+      },
+      {
+        name: "Family",
+        cost: 19.99,
+        amount: 6
+      }
+    ];
+
+
     this.userForm = new FormGroup({
       name: new FormControl(''),
       surname: new FormControl(''),
@@ -44,7 +46,7 @@ export class ShopComponent implements OnInit {
     })
   }
 
-  manageSubscription(userForm : NgForm) {
+  manageSubscription(userForm: FormGroup) {
     const newUser = userForm.value as User;
     this.user = {...newUser};
   }
